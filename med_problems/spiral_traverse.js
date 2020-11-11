168121711da987b1eaa94fc0fc932df97e48b13b
Daniel <danielgu00@gmail.com>
2020-11-11 12:47:24 -0800

    more problems

---
+++ med_problems/spiral_traverse.js
@@ -0,0 +1,32 @@
+function spiralTraverse(array) {
+  // Write your code here.
+    let res = [];
+
+    let startingRow = 0;
+    let startingCol = 0;
+    let endingRow = array.length - 1;
+    let endingCol = array[0].length - 1;
+
+    while(startingRow <= endingRow && startingCol <= endingCol) {
+        for(let i = startingCol; i <= endingCol; i++) {
+            res.push(array[startingRow][i]);
+        }
+        for(let i = startingRow + 1; i <= endingRow; i++) {
+            res.push(array[i][endingCol]);
+        }
+        for(let i = endingCol - 1; i >= startingCol; i--) {
+          if(startingRow === endingRow) break;  
+					res.push(array[endingRow][i]);
+					
+        }
+        for(let i = endingRow - 1; i > startingRow; i--) {
+          if(startingCol === endingCol) break;  
+					res.push(array[i][startingCol]);
+        }
+		startingRow++;
+        startingCol++;
+        endingRow--;
+        endingCol--;
+    }
+	return res;
+}
