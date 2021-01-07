d70f13c791a2231bcd8e0b84ccf4bc0ada185761
Daniel <danielgu00@gmail.com>
2021-01-07 14:25:22 -0800

    more problems

--- easy_problems/insertion_sort.js
+++ easy_problems/insertion_sort.js
@@ -12,3 +12,4 @@ function insertionSort(array) {
 
 // Do not edit the line below.
 exports.insertionSort = insertionSort;
+//hi
---
+++ med_problems/bst_construction.js
@@ -0,0 +1,34 @@
+// Do not edit the class below except for
+// the insert, contains, and remove methods.
+// Feel free to add new properties and methods
+// to the class.
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    if(this.value === null) {
+        this.value = value;
+    }
+    else if(this.value )
+    return this;
+  }
+
+  contains(value) {
+    // Write your code here.
+  }
+
+  remove(value) {
+    // Write your code here.
+    // Do not edit the return statement of this method.
+    return this;
+  }
+}
+
+// Do not edit the line below.
+exports.BST = BST;
