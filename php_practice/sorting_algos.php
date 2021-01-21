46a9030ff660db58228b258b42170d02c59597da
Daniel <danielgu00@gmail.com>
2021-01-20 21:54:41 -0800

    more problems

---
+++ php_practice/sorting_algos.php
@@ -0,0 +1,60 @@
+<?php
+// ********************* MERGE SORT ***********************************
+
+ function mergeSort($array) {
+
+   if(count($array) == 1) {
+     return $array;
+   }
+   $half = floor(count($array)/2);
+   $left = array_slice($array, 0, $half);
+   $right = array_slice($array, $half);
+
+  $result1 = mergeSort($left);
+  $result2 = mergeSort($right);
+  return merge($result1, $result2);
+ }
+ function merge($array1, $array2) {
+   
+   $new_arr = array();
+   while (!empty($array1) and !empty($array2)) {
+     if ($array1[0] > $array2[0]) {
+        array_push($new_arr, array_shift($array2));
+     }else {
+       array_push($new_arr, array_shift($array1));
+     }
+   }
+ 
+   if(empty($array1) and empty($array2)) {
+     return $new_arr;
+   }else if(empty($array1)) {
+     return array_merge($new_arr, $array2);
+   }else {
+     return array_merge($new_arr, $array1);
+   }
+ }
+
+ // ************************** QUICK SORT *************************************
+     function quicksort($array)
+    {
+        if (count($array) == 0)
+            return array();
+ 
+        $pivot_element = $array[0];
+        $left_element = $right_element = array();
+ 
+        for ($i = 1; $i < count($array); $i++) {
+          echo $pivot_element;
+            if ($array[$i] <$pivot_element) {
+                $left_element[] = $array[$i];
+                print_r($left_element);
+            }
+            else {
+                $right_element[] = $array[$i];
+                print_r($right_element);
+            }
+        }
+ 
+        return array_merge(quicksort($left_element), array($pivot_element), quicksort($right_element));
+    }
+?>
